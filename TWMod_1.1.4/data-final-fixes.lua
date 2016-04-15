1425ff633f2d1464b5fc1d28f234d31256bd147a
crazy2be <code@jmcgirr.com>
2016-04-14 23:09:08 -0400

    Actually make stack sizes 4x.

--- TWMod_1.1.4/data-final-fixes.lua
+++ TWMod_1.1.4/data-final-fixes.lua
@@ -27,40 +27,44 @@ function pretty(value,htchar,lfchar,indent)
     return tostring(value)
   end
 end
+
+-- This seems to get called twice, so the multipied value here should
+-- be the sqrt of what you really want. I.e. * 2 gives you 4x the stack
+-- size.
 function increase_stack(val)
 	-- We need the < 10000, because some items seem to have really
 	-- huge default stack values (MAX_INT?), and multiplying them by
 	-- 4 causes everything to break.
 	if val.stack_size and val.stack_size > 1 and val.stack_size < 10000 then
-		val.stack_size = val.stack_size * 4
+		val.stack_size = val.stack_size * 2
 	end
 end
 
+print(pretty(data.raw))
 for _,dat in pairs(data.raw) do
    for _,items in pairs(dat) do
 		increase_stack(items)
-		--print(pretty(items))
+		print(pretty(items))
 		--print(items.stack_size)
          --items.stack_size = items.stack_size * 4
 		 --print(items.stack_size)
    end
-   for i, ammo in pairs(data.raw.ammo) do
-	   increase_stack(ammo)
-      --if ammo.stack_size and ammo.stack_size>1 then
-	--	-ammo.stack_size = math.max(ammo.stack_size * 4, 1)
-	  --end
-	  --if ammo.magazine_size then
-		--ammo.magazine_size = ammo.magazine_size * 4
-	 -- end
-	end
-	for i, mod in pairs(data.raw["module"]) do
-      increase_stack(mod)
-    end
-	for i, cap in pairs(data.raw["capsule"]) do
-      increase_stack(cap)
-    end
-
 end
+--for i, ammo in pairs(data.raw.ammo) do
+--	increase_stack(ammo)
+	--if ammo.stack_size and ammo.stack_size>1 then
+--	-ammo.stack_size = math.max(ammo.stack_size * 4, 1)
+	--end
+	--if ammo.magazine_size then
+	--ammo.magazine_size = ammo.magazine_size * 4
+	-- end
+--end
+--for i, mod in pairs(data.raw["module"]) do
+--	increase_stack(mod)
+--end
+--for i, cap in pairs(data.raw["capsule"]) do
+--	increase_stack(cap)
+--end
 
 --Special thanks to Dysoch (DyTech) for this fix!
 
