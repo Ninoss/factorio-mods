2e5cd55d5c3613083e74b6611a30b7c830c91455
crazy2be <code@jmcgirr.com>
2016-04-21 01:41:55 -0400

    Try just changing the stack size of a few select items.
    Hopefully this fixes desyncs?

--- TWMod_1.1.4/data.lua
+++ TWMod_1.1.4/data.lua
@@ -28,6 +28,237 @@ function pretty(value,htchar,lfchar,indent)
   end
 end
 
+-- All items and 2x default stack sizes.
+-- stone-brick: 200
+-- raw-wood: 100
+-- coal: 100
+-- stone: 100
+-- iron-ore: 100
+-- copper-ore: 100
+-- wood: 100
+-- iron-plate: 200
+-- copper-plate: 200
+-- iron-stick: 200
+-- iron-gear-wheel: 200
+-- copper-cable: 400
+-- electronic-circuit: 400
+-- wooden-chest: 100
+-- stone-furnace: 100
+-- burner-mining-drill: 100
+-- basic-mining-drill: 100
+-- basic-transport-belt: 100
+-- burner-inserter: 100
+-- basic-inserter: 100
+-- offshore-pump: 40
+-- pipe: 100
+-- boiler: 100
+-- steam-engine: 20
+-- small-electric-pole: 100
+-- radar: 100
+-- computer: 1
+-- small-plane: 1
+-- small-lamp: 100
+-- alien-artifact: 1000
+-- pipe-to-ground: 100
+-- assembling-machine-1: 100
+-- red-wire: 400
+-- green-wire: 400
+-- stone-wall: 100
+-- gun-turret: 100
+-- solar-panel-equipment: 40
+-- fusion-reactor-equipment: 40
+-- energy-shield-equipment: 100
+-- energy-shield-mk2-equipment: 100
+-- battery-equipment: 100
+-- battery-mk2-equipment: 100
+-- basic-laser-defense-equipment: 40
+-- basic-electric-discharge-defense-equipment: 40
+-- basic-exoskeleton-equipment: 20
+-- personal-roboport-equipment: 10
+-- night-vision-equipment: 40
+-- land-mine: 40
+-- iron-chest: 100
+-- steel-chest: 100
+-- smart-chest: 100
+-- fast-transport-belt: 100
+-- express-transport-belt: 100
+-- long-handed-inserter: 100
+-- fast-inserter: 100
+-- smart-inserter: 100
+-- assembling-machine-2: 100
+-- assembling-machine-3: 100
+-- solar-panel: 100
+-- diesel-locomotive: 10
+-- cargo-wagon: 10
+-- straight-rail: 200
+-- curved-rail: 100
+-- player-port: 100
+-- gate: 100
+-- car: 1
+-- tank: 1
+-- lab: 20
+-- train-stop: 20
+-- rail-signal: 100
+-- rail-chain-signal: 100
+-- steel-plate: 200
+-- basic-transport-belt-to-ground: 100
+-- fast-transport-belt-to-ground: 100
+-- express-transport-belt-to-ground: 100
+-- basic-splitter: 100
+-- fast-splitter: 100
+-- express-splitter: 100
+-- advanced-circuit: 400
+-- processing-unit: 200
+-- logistic-robot: 100
+-- construction-robot: 100
+-- logistic-chest-passive-provider: 100
+-- logistic-chest-active-provider: 100
+-- logistic-chest-storage: 100
+-- logistic-chest-requester: 100
+-- rocket-silo: 1
+-- roboport: 10
+-- coin: 100000
+-- big-electric-pole: 100
+-- medium-electric-pole: 100
+-- substation: 100
+-- basic-accumulator: 100
+-- steel-furnace: 100
+-- electric-furnace: 100
+-- basic-beacon: 20
+-- storage-tank: 100
+-- small-pump: 100
+-- pumpjack: 40
+-- oil-refinery: 20
+-- chemical-plant: 20
+-- sulfur: 100
+-- empty-barrel: 20
+-- crude-oil-barrel: 20
+-- solid-fuel: 100
+-- plastic-bar: 200
+-- engine-unit: 100
+-- electric-engine-unit: 100
+-- explosives: 100
+-- battery: 400
+-- flying-robot-frame: 100
+-- arithmetic-combinator: 100
+-- decider-combinator: 100
+-- constant-combinator: 100
+-- low-density-structure: 20
+-- rocket-fuel: 20
+-- rocket-control-unit: 20
+-- rocket-part: 10
+-- satellite: 1
+-- concrete: 200
+-- laser-turret: 100
+-- gunship: 1
+-- water-barrel: 20
+-- sulfuric-acid-barrel: 20
+-- lubricant-barrel: 20
+-- petroleum-gas-barrel: 20
+-- light-oil-barrel: 20
+-- heavy-oil-barrel: 20
+-- landfill2by2: 512
+-- landfill4by4: 512
+-- water-be-gone: 128
+-- water-bomb: 128
+-- letter-uc-a: 60
+-- letter-uc-b: 60
+-- letter-uc-c: 60
+-- letter-uc-d: 60
+-- letter-uc-e: 60
+-- letter-uc-f: 60
+-- letter-uc-g: 60
+-- letter-uc-h: 60
+-- letter-uc-i: 60
+-- letter-uc-j: 60
+-- letter-uc-k: 60
+-- letter-uc-l: 60
+-- letter-uc-m: 60
+-- letter-uc-n: 60
+-- letter-uc-o: 60
+-- letter-uc-p: 60
+-- letter-uc-q: 60
+-- letter-uc-r: 60
+-- letter-uc-s: 60
+-- letter-uc-t: 60
+-- letter-uc-u: 60
+-- letter-uc-v: 60
+-- letter-uc-w: 60
+-- letter-uc-x: 60
+-- letter-uc-y: 60
+-- letter-uc-z: 60
+-- letter-lc-a: 60
+-- letter-lc-b: 60
+-- letter-lc-c: 60
+-- letter-lc-d: 60
+-- letter-lc-e: 60
+-- letter-lc-f: 60
+-- letter-lc-g: 60
+-- letter-lc-h: 60
+-- letter-lc-i: 60
+-- letter-lc-j: 60
+-- letter-lc-k: 60
+-- letter-lc-l: 60
+-- letter-lc-m: 60
+-- letter-lc-n: 60
+-- letter-lc-o: 60
+-- letter-lc-p: 60
+-- letter-lc-q: 60
+-- letter-lc-r: 60
+-- letter-lc-s: 60
+-- letter-lc-t: 60
+-- letter-lc-u: 60
+-- letter-lc-v: 60
+-- letter-lc-w: 60
+-- letter-lc-x: 60
+-- letter-lc-y: 60
+-- letter-lc-z: 60
+-- letter-s-0: 60
+-- letter-s-1: 60
+-- letter-s-2: 60
+-- letter-s-3: 60
+-- letter-s-4: 60
+-- letter-s-5: 60
+-- letter-s-6: 60
+-- letter-s-7: 60
+-- letter-s-8: 60
+-- letter-s-9: 60
+-- letter-s-and: 60
+-- letter-s-equal: 60
+-- letter-s-exclamation: 60
+-- letter-s-op: 60
+-- letter-s-plate: 60
+-- letter-s-plus: 60
+-- letter-s-question: 60
+-- letter-s-slash: 60
+-- reverse-factory: 20
+-- robotic-network-combinator: 100
+-- TeleporterCore: 32
+-- Teleporter_Tier01: 4
+-- Teleporter_Tier02: 4
+-- Teleporter_Tier03: 4
+-- Teleporter_Tier04: 4
+-- Teleporter_Tier05: 4
+-- Teleporter_Tier06: 4
+-- Teleporter_Tier07: 4
+-- Teleporter_Tier08: 4
+-- Teleporter_Tier09: 4
+-- Teleporter_Tier10: 4
+-- Teleporter_Upgrade_02: 4
+-- Teleporter_Upgrade_03: 4
+-- Teleporter_Upgrade_04: 4
+-- Teleporter_Upgrade_05: 4
+-- Teleporter_Upgrade_06: 4
+-- Teleporter_Upgrade_07: 4
+-- Teleporter_Upgrade_08: 4
+-- Teleporter_Upgrade_09: 4
+-- Teleporter_Upgrade_10: 4
+-- smart-train-stop: 20
+-- smart-train-stop-proxy: 100
+-- smart-train-stop-proxy-cargo: 100
+-- stainless-steel-wagon: 10
+-- stainless-steel-plate: 200
+
 function increase_stack(val)
 	-- We need the < 10000, because some items seem to have really
 	-- huge default stack values (MAX_INT?), and multiplying them by
@@ -54,7 +285,9 @@ end
 --    end
 -- end
 for item_name, item in pairs(data.raw.item) do
-	increase_stack(item)
+	-- calling increase_stack here seems to cause desyncs, so let's try
+	-- just manually setting the sizes of a few items, below.
+	--increase_stack(item)
 	print(item_name .. ": " .. (item.stack_size or "nil"))
 		--increase_stack(items)
 		--print(pretty(items))
@@ -62,6 +295,10 @@ for item_name, item in pairs(data.raw.item) do
          --items.stack_size = items.stack_size * 4
 		 --print(items.stack_size)
 end
+data.raw.item['basic-transport-belt-to-ground'].stack_size = 500
+data.raw.item['iron-plate'].stack_size = 500
+data.raw.item['copper-plate'].stack_size = 500
+
 --print("AFTER CHANGES")
 --print(pretty(data.raw))
 --print("END AFTER")
